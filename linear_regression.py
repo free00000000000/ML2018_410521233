154c5fce26df435d7eff01472f0cda44670c4e5a
410521233 <410521233@gms.ndhu.edu.tw>
2018-04-27 16:03:56 +0800

    read image

---
+++ linear_regression.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import tensorflow as tf
+import numpy as np
+
+
+k1 = mpimg.imread('./Image_and_ImageData/key1.png')
+k2 = mpimg.imread('./Image_and_ImageData/key2.png')
+I = mpimg.imread('./Image_and_ImageData/I.png')
+E = mpimg.imread('./Image_and_ImageData/E.png')
+eprime = mpimg.imread('./Image_and_ImageData/Eprime.png')
+
+
+
+plt.imshow(I)
+plt.show()

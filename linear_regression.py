a334049fa8395b857e51c89ec4334432caed4463
410521233 <410521233@gms.ndhu.edu.tw>
2018-04-27 16:25:44 +0800

    reshape images

--- linear_regression.py
+++ linear_regression.py
@@ -10,7 +10,20 @@ I = mpimg.imread('./Image_and_ImageData/I.png')
 E = mpimg.imread('./Image_and_ImageData/E.png')
 eprime = mpimg.imread('./Image_and_ImageData/Eprime.png')
 
-
-
-plt.imshow(I)
+plt.imshow(I, cmap="gray")
 plt.show()
+
+
+#print(k1.shape)  # (300, 400)
+k1 = np.reshape(k1, [-1])  # (120000,)
+k2 = np.reshape(k2, [-1])
+I = np.reshape(I, [-1])
+E = np.reshape(E, [-1])
+eprime = np.reshape(eprime, [-1])
+
+k1 = np.expand_dims(k1, axis=0)
+k2 = np.expand_dims(k2, axis=0)
+I = np.expand_dims(I, axis=0)
+a = np.concatenate((k1, k2, I), axis=0)  # (3, 120000)
+
+print(a.shape)
